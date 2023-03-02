f0e6d4cecf5902952e176c2a4cd1f5e7a5e990d9
Huy Hoang <maxmad11092001@gmail.com>
2023-03-03 00:19:46 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from "react";
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Analytics from './pages/Analytics';
-import Comment from './pages/Comment';
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
+import Category from './pages/Category';
+import Department from './pages/Department';
+import Management from './pages/Management';
+import StaffSubmission from './pages/StaffSubmission';
+import Submission from './pages/Submission';
 import Sidebar from './components/Sidebar'
+import Role from './pages/Role'
+import Statistics from './pages/Statistics'
+
 function App() {
   return (
     <BrowserRouter>
       <Sidebar>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/comment" element={<Comment />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/productList" element={<ProductList />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/department" element={<Department />} />
+          <Route path="/management" element={<Management />} />
+          <Route path="/staffSubmission" element={<StaffSubmission />} />
+          <Route path="/submission" element={< Submission />} />
+          <Route path="/role" element={<Role />} />
+          <Route path="/statistics" element={<Statistics />} />
         </Routes>
       </Sidebar>
     </BrowserRouter>
---
+++ src/pages/StaffSubmission.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StaffSubmission = () => {
+    return (
+        <div>
+            <h1>StaffSubmission Page</h1>
+        </div>
+    );
+};
+
+export default StaffSubmission;
