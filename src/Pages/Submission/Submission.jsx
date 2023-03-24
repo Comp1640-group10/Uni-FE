c5fc4ca34e4861b7d60ddfacbc6590c535e2449a
Huy Hoang <maxmad11092001@gmail.com>
2023-03-24 14:45:30 +0700

    page

--- src/Pages/Submission/Submission.jsx
+++ src/Pages/Submission/Submission.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     FaTrash,
     FaEdit,
@@ -6,46 +6,102 @@ import {
 } from 'react-icons/fa'
 import './Submission.css'
 const Submission = () => {
-    const submissionitem = [
-        {
-            name: 'Idea about improving team work',
-            deadline_1: '2/26/2023 11:49:00PM',
-            deadline_2: '3/14/2022 11:18:00 AM',
-            delete: <FaTrash />,
-            edit: <FaEdit />,
-            detail: <FaInfoCircle />
-        },
-        {
-            name: 'heat electricity',
-            deadline_1: '2/26/2023 11:49:00PM',
-            deadline_2: '3/14/2022 11:18:00 AM',
-            delete: <FaTrash />,
-            edit: <FaEdit />,
-            detail: <FaInfoCircle />
-        },
-        {
-            name: 'Newest submission',
-            deadline_1: '2/26/2023 11:49:00PM',
-            deadline_2: '3/14/2022 11:18:00 AM',
-            delete: <FaTrash />,
-            edit: <FaEdit />,
-            detail: <FaInfoCircle />
-        },
+    const [submissionitem, setSubmissionItem] = useState([{
+        name: 'meet',
+        deadline_1: '03/11/2022',
+        deadline_2: '04/11/2022',
+    }]);
+    const [newAdd, setNewAdd] = useState({
+        name: '',
+        deadline_1: '',
+        deadline_2: '',
+    })
 
-    ]
+    const handleChange = (event) => {
+        event.preventDefault();
+        const fieldName = event.target.getAttribute('name');
+        const fieldValue = event.target.value;
+
+        const newFormAdd = { ...newAdd };
+        newFormAdd[fieldName] = fieldValue
+        setNewAdd(newFormAdd);
+
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newAddItem = {
+            name: newAdd.name,
+            deadline_1: newAdd.deadline_1,
+            deadline_2: newAdd.deadline_2
+        }
+        const newAddItems = [...submissionitem, newAddItem]
+        setSubmissionItem(newAddItems)
+
+    }
+    const deleteItem = (index) => {
+        let newAdds = submissionitem.splice(index, 1)
+        setNewAdd(newAdds);
+    }
     return (
 
         <div className=''>
-
-            <div className='category'>
-                <h3>Category Page</h3>
+            <h1>Submission page</h1>
+            <form onSubmit={handleSubmit} className="Role">
+                <div className="input-page">
+                    <div className="input_name">
+                        <h3>Name Submission</h3>
+                        <input
+                            type="text"
+                            placeholder="add new submission"
+                            size="50"
+                            maxLength="50"
+                            name='name'
+                            required='required'
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input_name">
+                        <h3>deadline 1</h3>
+                        <input
+                            type="date"
+                            placeholder="add new deadline_1"
+                            maxLength="50"
+                            name='deadline_1'
+                            required='required'
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input_name">
+                        <h3>deadline 2</h3>
+                        <input
+                            type="date"
+                            placeholder="add new deadline_2"
+                            maxLength="50"
+                            name='deadline_2'
+                            required='required'
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="btn">
+                        <button type='submit'>add new</button>
+                    </div>
+                </div>
+            </form>
+            <div className='Submission'>
+                <h3>Submission Page</h3>
+                {submissionitem && submissionitem.length ? '' : 'No Item...'}
                 {
                     submissionitem.map((submission, index) => (
                         <div className='icons'>
                             <div className='name'>{submission.name}</div>
                             <div className='deadline_1'>{submission.deadline_1}</div>
                             <div className='deadline_2'>{submission.deadline_2}</div>
-                            <div className='icon-cate'>{submission.edit} {submission.delete} {submission.detail}</div>
+                            <div className='icon-cate'>
+                                <span title="edit"><FaEdit /></span>
+                                <span title="delete" onClick={() => deleteItem(index)}><FaTrash /></span>
+                                <span title="detail" ><FaInfoCircle /></span>
+
+                            </div>
                         </div>
                     ))
 
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -5,11 +5,7 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Category from "../Pages/Categories/Category";
 import Department from "../Pages/Departments/Department";
-import Management from "../Pages/Management/Management";
-import NewCategory from "../Pages/Categories/NewCategory";
-import NewDepartment from "../Pages/Departments/NewDepartment";
 import Role from "../Pages/Role/Role";
-import NewRole from "../Pages/Role/NewRole";
 import StaffSubmission from "../Pages/StaffSubmission/StaffSubmission";
 import Statistics from "../Pages/Statistics/Statistics";
 import Submission from "../Pages/Submission/Submission";
@@ -17,14 +13,10 @@ import Submission from "../Pages/Submission/Submission";
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/category", component: Category },
-  { path: "/newcategory", component: NewCategory },
   { path: "/login", component: Login },
   { path: "/department", component: Department },
-  { path: "/newdepartment", component: NewDepartment },
   { path: "/role", component: Role },
-  { path: "/newrole", component: NewRole },
   { path: "/staffsubmission", component: StaffSubmission },
-  { path: "/management", component: Management },
   { path: "/statistics", component: Statistics },
   { path: "/submission", component: Submission },
 ];
